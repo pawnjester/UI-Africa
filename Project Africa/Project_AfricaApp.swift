25ba9d1c158882bdec777245feb7f87b2565c1d4
pawnjester <ebukeokonji@gmail.com>
2021-03-25 12:11:45 +0100

    Initial Commit

---
+++ Project Africa/Project_AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_AfricaApp.swift
+//  Project Africa
+//
+//  Created by okonji emmanuel on 25/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct Project_AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
